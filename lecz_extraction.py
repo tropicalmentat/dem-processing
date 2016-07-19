4925302d24074b3e1ce76a79b9a545b7c52da89f
tropicalhobo <jose.torres@obf.ateneo.edu>
2016-07-19 16:36:21 +0800

    fixed no-data mask

--- lecz_extraction.py
+++ lecz_extraction.py
@@ -2,13 +2,14 @@ import gdal, sys
 from gdalconst import *
 import numpy as np
 import time as tm
+import numpy.ma as ma
 
 start = tm.time()
 
 print 'Extracting Low Elevation Coastal Zones...\n'
 
-#open dataset
-fn = 'PANAYdem30m.tif'
+# open dataset
+fn = 'D:\LUIGI\ICLEI\GIS PROJECT FILES\LOW ELEVATION ZONES\\camsur_dem.tif'
 ds = gdal.Open(fn, GA_ReadOnly)
 
 if ds == None:
@@ -19,50 +20,40 @@ else:
     
 driver = ds.GetDriver()
 
-#retrieve image attributes
+# retrieve image attributes
 cols, rows = ds.RasterXSize, ds.RasterYSize
 proj, gt = ds.GetProjection(), ds.GetGeoTransform()
 iband = ds.GetRasterBand(1)
 
-#extract LECZ and create separate output datasets
-error = 5 
+# extract LECZ and create separate output datasets
+error = 5  # expected upper-bound error for geographical region
 
-for level in range(2,11,2):
-    ods = driver.Create('LECZ_' + str(level) + 'm' + '_UTM51N' + '.tif',cols,rows,GDT_Byte)
+for level in range(2, 11, 2):
+    # created data set for each lez level
+    ods = driver.Create('LECZ_' + str(level) + 'm' + '_UTM51N' + '.tif',
+                        cols, rows, GDT_Byte)
     ods.SetProjection(proj)
     ods.SetGeoTransform(gt)
     oband = ods.GetRasterBand(1)
 
-    #blocking
-    xbs = 1000
-    ybs = 1000
+    idata = iband.ReadAsArray(0, 0, cols, rows)
 
-    for i in range(0,rows,ybs):
-        if rows > i + ybs:
-            numrows = ybs
-        else:
-            numrows = rows - i
-        for j in range(0,cols,xbs):
-            if cols > j + xbs:
-                numcols = xbs
-            else:
-                numcols = cols - j
+    lez = np.where(idata < level + 1, idata, 0)  # low elevation zone
 
-            idata = iband.ReadAsArray(j,i,numcols,numrows)
+    no_data = np.where(idata > 0, False, True)  # locate no-data values and replace them with False
 
-            sealevel = np.where(idata == 0,1,0)
-            lecz = np.where(idata <= (level),level,0)
-            output = lecz - sealevel
-            oband.WriteArray(output,j,i)
-            oband.FlushCache()
+    sl_mask = ma.array(lez, mask=no_data, fill_value=-99)  # create no-data value mask
 
-    #memory management
+    oband.WriteArray(sl_mask)
+    oband.FlushCache()
+
+    # memory management
     ods = None
     oband = None
     idata = None
     lecz = None
     
-#memory management
+# memory management
 ds = None
 iband = None
 
